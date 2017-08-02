7b6499c3ff96faaf04f7c9f5a6801676b5c2dc6e
dgsousa <sousa.danielg@gmail.com>
2017-08-02 07:10:17 -0700

    config vars

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 
 
 const config = {
-	apiKey: "AIzaSyCxwT2udbQFddsw-_zxoum0lJlJ1VR_rAg",
-    authDomain: "voting-app-9a2b1.firebaseapp.com",
-    databaseURL: "https://voting-app-9a2b1.firebaseio.com",
-    projectId: "voting-app-9a2b1",
-    storageBucket: "voting-app-9a2b1.appspot.com",
-    messagingSenderId: "517098115982"
+	apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGE_SENDER_ID
 };
 
 module.exports = config;
