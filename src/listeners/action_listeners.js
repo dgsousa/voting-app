9b6dabf9c4b4dfa46e688172415673fb7ba491ba
dgsousa <sousa.danielg@gmail.com>
2017-08-07 14:52:42 -0700

    adjustments

--- src/listeners/action_listeners.js
+++ src/listeners/action_listeners.js
@@ -2,16 +2,11 @@
 
 const voteActionListener = (database, data) => {
 	const {id, option, user} = data;
-	database.ref("polls/" + id).transaction(state => {
+	database.ref("polls/" + id).transaction(state => ({
 		...state,
 		voted: {...state.voted, [user]: true},
-		options: {...state.options, [option]: (state.options[option] + 1) || 1},
-
-
-		// const voted = Object.assign(state.voted || {}, {[user]: true})
-		// const options = Object.assign(state.options, {[option]: (state.options[option] + 1) || 1})
-		// return Object.assign(state, {voted}, {options});
-	})
+		options: {...state.options, [option]: (state.options[option] + 1) || 1}
+	}))
 }
 
 const addPollActionListener = (database, data) => {
