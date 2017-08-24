eabf0e172d1da9b778940ea0d7e2aaf4ea55a64e
dgsousa <sousa.danielg@gmail.com>
2017-08-24 12:40:33 -0700

    setting up sockets

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 "use strict";
 
-
+//Packages
 const express = require("express");
 const app = express();
 const path = require("path");
 const server = require("http").createServer(app);
 const favicon = require("serve-favicon");
 const firebase = require("firebase");
+const session = require("express-session")({
+    secret: "firebase",
+    resave: true,
+    saveUninitialized: true
+})
+const sharedSession = require("express-socket.io-session")(session);
 
+//Files
 const socketServer = require("./socket/socket_server.js");
 const database = require("./database");
-const port = process.env.PORT || 3000;
 const index = path.join(__dirname + "/views/index.ejs");
 
 
 
+
+
+//Send Static Files
 app.use("/static", express.static("public"));
 app.use(favicon('./public/images/vote.png'));
 
@@ -24,7 +33,13 @@ app.use("/", (req, res) => {
 	res.render(index, {});
 });
 
-server.listen(port);
+//Set up session
+app.use(session);
+
+
+
+
+server.listen(process.env.PORT || 3000);
 
 const socket = socketServer(server, database);
 
