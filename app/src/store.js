8950301a022e90313a5b0124b5011044e9bbd22c
dgsousa <sousa.danielg@gmail.com>
2017-09-08 13:33:22 -0700

    rebuild

--- app/src/store.js
+++ app/src/store.js
@@ -1,4 +1,4 @@
-aimport {createStore, applyMiddleware, compose} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
 import appReducer from "../reducers/reducer.js";
 
