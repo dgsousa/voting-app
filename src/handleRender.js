574552ffc78a04eee3cff0e6fb6f2260ae5d1218
dgsousa <sousa.danielg@gmail.com>
2017-09-08 17:59:12 -0700

    adjustments

--- src/handleRender.js
+++ src/handleRender.js
@@ -37,8 +37,6 @@ const handleRender = (file, database) => async (req, res) => {
 		</Provider>
 	)
 
-	console.log(reactComponent)
-
 	context.url ? 
 		res.writeHead(302, { Location: 302 }) : 
 		res.status(200).render(file, {reactComponent, initialState});
