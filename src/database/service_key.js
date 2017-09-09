36bfebcb4e87e6b79fb676f37adc0e96a815b839
dgsousa <sousa.danielg@gmail.com>
2017-09-09 11:50:47 -0700

    adjustments

--- src/database/service_key.js
+++ src/database/service_key.js
@@ -1,29 +1,3 @@
-const pkl1 = process.env.PKL1;
-const pkl2 = process.env.PKL2;
-const pkl3 = process.env.PKL3;
-const pkl4 = process.env.PKL4;
-const pkl5 = process.env.PKL5;
-const pkl6 = process.env.PKL6;
-const pkl7 = process.env.PKL7;
-const pkl8 = process.env.PKL8;
-const pkl9 = process.env.PKL9;
-const pkl10 = process.env.PKL10;
-const pkl11 = process.env.PKL11;
-const pkl12 = process.env.PKL12;
-const pkl13 = process.env.PKL13;
-const pkl14 = process.env.PKL14;
-const pkl15 = process.env.PKL15;
-const pkl16 = process.env.PKL16;
-const pkl17 = process.env.PKL17;
-const pkl18 = process.env.PKL18;
-const pkl19 = process.env.PKL19;
-const pkl20 = process.env.PKL20;
-const pkl21 = process.env.PKL21;
-const pkl22 = process.env.PKL22;
-const pkl23 = process.env.PKL23;
-const pkl24 = process.env.PKL24;
-const pkl25 = process.env.PKL25;
-const pkl26 = process.env.PKL26;
 
 const getApiKey = () => {
 	let key = "";
@@ -48,17 +22,3 @@ module.exports = {
 	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
 	"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-eo9zs%40voting-app-9a2b1.iam.gserviceaccount.com"
 }
-
-
-// module.exports = {
-//   "type": "service_account",
-//   "project_id": "voting-app-9a2b1",
-//   "private_key_id": "5c8c6a7483a6d7a8c33af144dd12c97b1d9c0dce",
-//   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC7PiA6hMjPqVfk\n3uPkviS0dCbyPiAVfxR8QNrBJwSm6WFQoIGgLUiRXF+x8h9SNkjvwcAfbF/6/DVi\n1EwMQv00Oo5KPxPcJPi9I2XVX1rt+unUuFLhaMaUGaVi5WYyW9YT06UND16oW7Ki\nK0YVwl9rYSQMTJGfJsbWykC+3FMsq2hf7/DXomNtqIsd8mgl83xhy/8EcttkI/Vn\nHU1EeR2DeELv69LYmRFaZz+DQFiA9gF4qS+pYY2IDswDnuBbF4AHWlC7/JQ0gPhN\nRtnQrPlTX+pkj/7lrGzq2RqhzxZ7TYDybMqJWFeZWhlR9JX2zePYI2tPNmCUqLs0\nEx92Yyz5AgMBAAECggEADVm8e4pXNR31HeUEk8fjnxg/D1D9rFAKJQ03a8Pn8YIS\nGxPEBP3llDO6wQSAq4siVTICjFsHrEIfs63BGbNhvBUcFdAnOYWgqf+5oDvzJE/F\nLL0ob65ZplqpGlg9RVmbSyub3kavxZ+lfimeuX4em+3qX5QgDUclsKVCy45dwdam\n3xy94IzkeWPs1Q+ULapnjw1gD4xTW0uo6WyWGSDl9or3xIFpsrX8UuwB62uf3tpg\n2N1/asXUJMGS/RHZkuu/wG75sehrbQ4qcCrMEcLvmqlTru8F9x1kFFP9Av1B5wRL\niBPZ9WPMmwkX/ZcN7nE1oi+cBiu6OLHvM1pHp4XY9wKBgQDxhxLyXnukQvIdeeng\njMzK5Ya0KCdk78PquK8I4YpjSwl65tI4RgacFx78uhy3p70qLJOJB3vgtlLbi2tP\nMziBhlAsBQhkeTQFElXZgZi3vJdc1ZjPOY8oAqwvNVzU+iVUZhNc1ZRYeoNBvPjt\n3cyZBo/9KE31JDnS682hXR6qPwKBgQDGdlhOUwP5sFjmBZnHQP0iyrLbETtxghkM\nLvAGCez/JuofElueDcEyWSs70FBFZU+vNXU1GoUMafy+06C1aI3d41RL8mqaiS+O\n9wza/PczRD6taSAZAuILm7znGMskFEeZfE3GkXAfEau79du3SEkjaCEcQsU59F1Y\nbeHbY8GqxwKBgCupFjS3/bPQ4kGP00ZjTXX0DeitRYnKiej5X7LESqbuXpVX+nJv\nviwjRde7sQtGCD0Cd90SfEM/0jSbvFdB1Bod9IowkrbO7idojBpYggnczqPafNgg\n+I7sBMI37EMXwP3DwgZLGZjw9J9CkOE28hBLjmXeDlGdLdLCC5b65VszAoGBAJRg\nBDNCo3U4n64RP+AVOWuaic94ea2qK1nNecu5/gLgsCbczbxD4k3nFoiYfHr1fHas\ni91Q32nhj4GHrN4TtgTPIcXKDmNM705+a5EJkTM18XoqX0HzwtYU8E0CX1WxB7q7\nkVZEg8PmrTNQY50KoQuknSSI0vU6OdOUC0i/sxofAoGAFsJiJ2zz7BvWQYfAJ7Ha\npy/jeQhRvME43rKSnalLq8thKzSb1ODwbmCsbz+2zGkWwaDJuhkorkWVrWuzW9bn\nco9UX1wbgC0x6+Hf25DQlI35pfXrebJxoiGcyzGb2Wzsz824cl2hRjnV7eIpr4J5\nm1yD4G0hcyK7zRx7dKXkHuA=\n-----END PRIVATE KEY-----\n",
-//   "client_email": "firebase-adminsdk-eo9zs@voting-app-9a2b1.iam.gserviceaccount.com",
-//   "client_id": "107222254891823127170",
-//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-//   "token_uri": "https://accounts.google.com/o/oauth2/token",
-//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-//   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-eo9zs%40voting-app-9a2b1.iam.gserviceaccount.com"
-// }
