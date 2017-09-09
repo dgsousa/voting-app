5d54bd9030e66186c6d4438ea46a7edd6e3a275b
dgsousa <sousa.danielg@gmail.com>
2017-09-09 11:53:56 -0700

    adjustments

--- src/database/service_key.js
+++ src/database/service_key.js
@@ -13,10 +13,10 @@ const key = getApiKey();
 module.exports = {
 	"type": "service_account",
 	"project_id": "voting-app-9a2b1",
-	"private_key_id": "4620d74309b3c80719c68aa4aae495de3f979291", //stored in heroku
+	"private_key_id": process.env.SERVICE_PRIVATE_KEY_ID, //stored in heroku
 	"private_key": `-----BEGIN PRIVATE KEY-----\n${key}-----END PRIVATE KEY-----\n`, //stored in heroku
 	"client_email": "firebase-adminsdk-eo9zs@voting-app-9a2b1.iam.gserviceaccount.com",
-	"client_id": "107222254891823127170", //stored in heroku
+	"client_id": process.env.SERVICE_CLIENT_ID, //stored in heroku
 	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
 	"token_uri": "https://accounts.google.com/o/oauth2/token",
 	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
